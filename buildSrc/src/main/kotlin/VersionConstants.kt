5fd43070a139c8435b3211c97c97460a2474560d
Roman Zhuravlev <rozhur@zhdev.org>
2025-11-02 04:09:45 +0500

    Use newer adventure api version

--- buildSrc/src/main/kotlin/VersionConstants.kt
+++ buildSrc/src/main/kotlin/VersionConstants.kt
@@ -1,7 +1,7 @@
 object VersionConstants {
     const val cloudVersion = "1.8.5-SNAPSHOT"
-    const val adventureVersion = "4.11.0"
-    const val adventurePlatformVersion = "4.1.2"
-    const val adventureMinimessageVersion = "4.2.0-SNAPSHOT"
+    const val adventureVersion = "4.14.0"
+    const val adventurePlatformVersion = "4.4.1"
+    const val adventureMinimessageVersion = "4.14.0"
     const val bstatsVersion = "3.0.0"
 }
